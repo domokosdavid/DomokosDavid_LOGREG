01767269b93f7a9a8cfc18f9aaaeaa6d8805adab
Domokos David <domokosdavid4@gmail.com>
2021-12-01 20:05:27 +0100

    bejelentkezes

--- app/src/main/java/com/example/logreg/LoggedInActivity.java
+++ app/src/main/java/com/example/logreg/LoggedInActivity.java
@@ -2,27 +2,33 @@ package com.example.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoggedInActivity extends AppCompatActivity {
     private Button btnKijelentkezes;
     private TextView felhasznaloNeve;
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_logged_in);
 
         init();
+        felhasznaloNeve.setText(R.id.reg_felhasznev);
         btnKijelentkezes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent kijelentkezesre = new Intent(LoggedInActivity.this, MainActivity.class);
                 startActivity(kijelentkezesre);
+                Toast.makeText(getApplicationContext(), "Sikeresen kijelentkeztél", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
--- app/src/main/java/com/example/logreg/MainActivity.java
+++ app/src/main/java/com/example/logreg/MainActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnRegisztracio;
     private Button btnBejelentkezes;
+    private EditText logFelhNev;
+    private EditText logJelszo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnBejelentkezes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(R.id.reg_felhasznev == R.id.log_felhnev && R.id.reg_jelszo == R.id.log_jelszo){
+                    Intent bejelentkezesre = new Intent(MainActivity.this, LoggedInActivity.class);
+                    startActivity(bejelentkezesre);
+                    finish();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Hibás adatok!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
     private void init(){
         btnRegisztracio = findViewById(R.id.btn_regisztracio);
         btnBejelentkezes = findViewById(R.id.btn_bejelentkezes);
+        logFelhNev = findViewById(R.id.log_felhnev);
+        logJelszo = findViewById(R.id.log_jelszo);
     }
 }
