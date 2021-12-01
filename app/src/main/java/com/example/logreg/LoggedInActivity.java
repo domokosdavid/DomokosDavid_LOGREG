fb2f2e0362ba884075834a38f7f8d3d221516c55
Domokos David <domokosdavid4@gmail.com>
2021-12-01 19:28:09 +0100

    logged in act. letrehozas, gomb funkcio

--- app/src/main/java/com/example/logreg/LoggedInActivity.java
+++ app/src/main/java/com/example/logreg/LoggedInActivity.java
@@ -2,13 +2,34 @@ package com.example.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoggedInActivity extends AppCompatActivity {
+    private Button btnKijelentkezes;
+    private TextView felhasznaloNeve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        init();
+        btnKijelentkezes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kijelentkezesre = new Intent(LoggedInActivity.this, MainActivity.class);
+                startActivity(kijelentkezesre);
+                finish();
+            }
+        });
+    }
+
+    private void init(){
+        btnKijelentkezes = findViewById(R.id.btnKijelentkezes);
+        felhasznaloNeve = findViewById(R.id.felh_neve);
     }
 }
--- app/src/main/java/com/example/logreg/RegisterActivity.java
+++ app/src/main/java/com/example/logreg/RegisterActivity.java
@@ -2,13 +2,35 @@ package com.example.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Button regRegisztracio;
+    private Button regVissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        init();
+
+        regVissza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent visszara = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(visszara);
+                finish();
+            }
+        });
+
+}
+    private void init(){
+        regRegisztracio = findViewById(R.id.reg_Regisztracio);
+        regVissza = findViewById(R.id.reg_Vissza);
     }
 }
