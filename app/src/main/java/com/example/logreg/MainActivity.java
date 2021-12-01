de8c0a9aba73f4183a6c741d20dd69e2a610f8dc
Domokos David <domokosdavid4@gmail.com>
2021-12-01 19:14:38 +0100

    gombok, edit text letrehoz, regisztracio gomb atvisz a registry act.-ra

--- app/src/main/java/com/example/logreg/MainActivity.java
+++ app/src/main/java/com/example/logreg/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnRegisztracio;
+    private Button btnBejelentkezes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        btnRegisztracio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regisztraciora = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(regisztraciora);
+                finish();
+            }
+        });
+
+    }
+    private void init(){
+        btnRegisztracio = findViewById(R.id.btn_regisztracio);
+        btnBejelentkezes = findViewById(R.id.btn_bejelentkezes);
     }
 }
